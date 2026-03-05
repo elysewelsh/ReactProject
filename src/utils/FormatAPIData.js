84fac72eeb0f51f453587ed58735c204570353a6
elysewelsh <elysecwelsh@gmail.com>
2026-03-05 12:43:14 -0500

    started re-writing formatAPIData to remove state/context references

--- src/utils/FormatAPIData.js
+++ src/utils/FormatAPIData.js
@@ -16,52 +16,63 @@ export default function formatAPIdata (data) {
                 timezone: '',
                 zip: ''
             });
-        data.location.lat &&
+
             setAPIData({
-                ...prevAPIData, 
-                latitude: data.location.lat
+                latitude: data.location.lat? data.location.lat : 0,
+                longitude: data.location.lng? data.location.lng : 0,
+                isp: data.isp? data.isp : '',
+                ip: data.ip? data.ip : '',
+                city: data.location.city? data.location.city : '',
+                state: data.location.region? data.location.region : '',
+                timezone: data.location.timezone? data.location.timezone : '',
+                zip: data.location.postalcode? data.location.postalcode : ''
             });
-        data.location.lng &&
-            setAPIData({
-                ...prevAPIData, 
-                longitude: data.location.lng
-            });
-        data.location.city &&
-            setAPIData({
-                ...prevAPIData, 
-                city: data.location.city
-            });
-        data.location.region &&
-            setAPIData({
-                ...prevAPIData, 
-                state: data.location.region
-            });
-        data.location.country &&
-            setAPIData({
-                ...prevAPIData, 
-                country: data.location.country
-            });
-        data.location.postalcode &&
-            setAPIData({
-                ...prevAPIData, 
-                zip: data.location.postalcode
-            });
-        data.location.timezone &&
-            setAPIData({
-                ...prevAPIData, 
-                timezone: data.location.timezone
-            });
-        data.ip &&
-            setAPIData({
-                ...prevAPIData, 
-                ip: data.ip
-            });
-        data.isp &&
-            setAPIData({
-                ...prevAPIData, 
-                isp: data.isp
-            });
-        } else {
-            setError("API data is undefined");
-        }
+        // data.location.lat &&
+        //     setAPIData({
+        //         ...prevAPIData, 
+        //         latitude: data.location.lat
+        //     });
+        // data.location.lng &&
+        //     setAPIData({
+        //         ...prevAPIData, 
+        //         longitude: data.location.lng
+        //     });
+        // data.location.city &&
+        //     setAPIData({
+        //         ...prevAPIData, 
+        //         city: data.location.city
+        //     });
+        // data.location.region &&
+        //     setAPIData({
+        //         ...prevAPIData, 
+        //         state: data.location.region
+        //     });
+        // data.location.country &&
+        //     setAPIData({
+        //         ...prevAPIData, 
+        //         country: data.location.country
+        //     });
+        // data.location.postalcode &&
+        //     setAPIData({
+        //         ...prevAPIData, 
+        //         zip: data.location.postalcode
+        //     });
+        // data.location.timezone &&
+        //     setAPIData({
+        //         ...prevAPIData, 
+        //         timezone: data.location.timezone
+        //     });
+        // data.ip &&
+        //     setAPIData({
+        //         ...prevAPIData, 
+        //         ip: data.ip
+        //     });
+        // data.isp &&
+        //     setAPIData({
+        //         ...prevAPIData, 
+        //         isp: data.isp
+        //     });
+        // } else {
+        //     setError("API data is undefined");
+        // }
 };
